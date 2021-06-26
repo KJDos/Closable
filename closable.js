c3e9289071e880f981abbaa20e4b9846f1946ade
KJDos <hassan.allalou@gmail.com>
2021-06-26 10:12:41 +0200

    first commit

---
+++ closable.js
@@ -0,0 +1,7 @@
+var closeBtn = document.querySelectorAll('.js-close');
+for (let i = 0; i < closeBtn.length; i++) {
+    closeBtn[i].addEventListener("click", function(){
+        var target = document.querySelector('#'+closeBtn[i].dataset.target);
+        target.style.display="none"
+    })
+}
